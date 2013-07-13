da8dabba19fb7754da50b02eef67f5e4720dca80
Dan Moseley <danomoseley@gmail.com>
2013-07-13 12:32:39 -0400

    Preload lower quality image

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
     $backgroundImages = array(
-        'images/20130611_1023.jpg',
-        'images/20130611_1124.jpg',
-        'images/20130702_1383.jpg',
-        'images/20130611_1034.jpg',
-        'images/20130611_1151.jpg',
-        'images/20130702_1385.jpg',
-        'images/20130611_1069.jpg',
-        'images/20130702_1350.jpg',
-        'images/20130702_1404.jpg'
+        '20130611_1023.jpg',
+        '20130611_1124.jpg',
+        '20130702_1383.jpg',
+        '20130611_1034.jpg',
+        '20130611_1151.jpg',
+        '20130702_1385.jpg',
+        '20130611_1069.jpg',
+        '20130702_1350.jpg',
+        '20130702_1404.jpg'
     );
     $lastBackgroundImageId = $_COOKIE['backgroundImageId'];
     if (empty($lastBackgroundImageId)) {
@@ -27,12 +27,14 @@
         <link rel="stylesheet" href="css/style.css" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
         <script src="//cdn.jsdelivr.net/jquery.cookie/1.3.1/jquery.cookie.js"></script> 
+        <script>window.backgroundImageSrc = '<?php echo $backgroundImage; ?>';</script>
         <script src="js/main.js"></script>
         <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
     </head>
     <body>
-        <div id="stretch-background-container" style="background-image:url(<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
+        <div id="cover-background-container" style="background-image:url(images/<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
+        <div id="preload-cover-background-container" style="background-image:url(images/smaller/<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(window).load(function(){
-    $('body').fadeIn();
-});
+var preloadImg = new Image();
+preloadImg.onload = function() {
+    $('#preload-cover-background-container').fadeIn();
+};
+preloadImg.src = "images/" + window.backgroundImageSrc;
+
+var mainImg = new Image();
+mainImg.onload = function() {
+    $('#cover-background-container').fadeIn();
+    $('#preload-cover-background-container').remove();
+};
+mainImg.src = "images/preload/" + window.backgroundImageSrc;
