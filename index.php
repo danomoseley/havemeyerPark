41109c39f34d55362850539146dd798d36ec374a
Dan Moseley <danomoseley@gmail.com>
2013-07-13 13:12:09 -0400

    Add white border

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
         <![endif]-->
     </head>
     <body>
-        <div id="cover-background-container" style="background-image:url(images/<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
-        <div id="preload-cover-background-container" style="background-image:url(images/preload/p_<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
+        <div class="border-box" id="cover-background-container" style="background-image:url(images/<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
+        <div class="border-box" id="preload-cover-background-container" style="background-image:url(images/preload/p_<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
     </body>
 </html>
