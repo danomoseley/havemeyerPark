bb69b3888e2aa38b51a4c8bc3cd0c21de49a43be
Dan Moseley <danomoseley@gmail.com>
2013-07-13 11:43:12 -0400

    Hide page until fully loaded

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 ?>
 
 <!doctype html>
-<html lang="en" style="background-image:url(<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
+<html lang="en" style="visibility:hidden;background-image:url(<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
     <head>
         <meta charset="utf-8" />
         <title>Havemeyer Park</title>
---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(window).load(function(){
+    $('html').css("visibility", "visible");
+});
