95edc053984a203effb70abaf487018e85190245
Dan Moseley <danomoseley@gmail.com>
2013-07-14 11:32:49 -0400

    Cleanup code

--- index.php
+++ index.php
@@ -10,16 +10,10 @@
         '20130702_1350.jpg',
         '20130702_1404.jpg'
     );
-    $lastBackgroundImageId =  isset($_COOKIE['backgroundImageId']) ? $_COOKIE['backgroundImageId'] : null;
-    if (empty($lastBackgroundImageId)) {
-        $lastBackgroundImageId = 0;
-    }
-    $lastBackgroundImageId++;
-    $backgroundImage = $backgroundImages[$lastBackgroundImageId%count($backgroundImages)];
+    $backgroundViewCount =  isset($_COOKIE['backgroundViewCount']) ? $_COOKIE['backgroundViewCount'] : 0;
+    setcookie('backgroundViewCount', ++$backgroundViewCount);
 
-    setcookie('backgroundImageId', $lastBackgroundImageId);
-    setcookie('backgroundImages', json_encode($backgroundImages));
-    setcookie('backgroundImageSrc', $backgroundImage);
+    $backgroundImage = $backgroundImages[$backgroundViewCount%count($backgroundImages)];
 ?>
 
 <!doctype html>
@@ -30,18 +24,18 @@
         <link rel="stylesheet" href="css/style.css" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
         <script src="//cdn.jsdelivr.net/jquery.cookie/1.3.1/jquery.cookie.js"></script>
+        <script>window.backgroundImages = <?php echo json_encode($backgroundImages) ?>;</script>
         <script src="js/main.js"></script>
         <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
     </head>
     <body>
-        <div class="border-box content" id="cover-background-container" style="background-image:url(images/<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
+        <div class="border-box content" id="cover-background-container" style="background-image:url(images/preload/p_<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;">
             <div class="border border-box ">
                 <div class="name border-box"><span class="letter-spaced">HAVEMEYERPAR</span><span class="last">K</span></div>
             </div>
         </div>
-        <div class="border-box" id="preload-cover-background-container" style="background-image:url(images/preload/p_<?php echo $backgroundImage; ?>);-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;"></div>
         <div id="sponsors">
             <a target="_blank" href="http://northbrooklynfarms.com"><img src="logos/nbk.png"/></a>
             <a target="_blank" href="https://twitter.com/bbbyrdd"><img src="logos/redcircle.png"/></a>
--- js/main.js
+++ js/main.js
@@ -1,39 +1,59 @@
-var preloadImg = new Image();
-var backgroundImageSrc = $.cookie('backgroundImageSrc');
-var backgroundImageId = $.cookie('backgroundImageId');
-var backgroundImages = JSON.parse($.cookie('backgroundImages'));
-preloadImg.onload = function() {
-    $('#preload-cover-background-container').fadeIn();
-};
-preloadImg.src = "images/" + backgroundImageSrc;
+backgroundViewCount = $.cookie('backgroundViewCount');
+currentBackgroundImageSrc = backgroundImages[backgroundViewCount%backgroundImages.length]
 
-var mainImg = new Image();
-mainImg.onload = function() {
-    $('#cover-background-container').show();
-    $('#sponsors').show();
-    $('#preload-cover-background-container').remove();
-    resizeText();
-};
-mainImg.src = "images/preload/p_" + backgroundImageSrc;
+setBackgroundImage({
+    'source': "preload/p_" + currentBackgroundImageSrc,
+    'transition': false,
+    'callback': function() {
+        $('#sponsors').show();
+        resizeText();
+        setBackgroundImage({
+            'source': currentBackgroundImageSrc,
+            'transition': false
+        });
+    }
+});
 
 window.setInterval(function() {
-    $.cookie('backgroundImageId', ++backgroundImageId);
-    backgroundImageSrc = backgroundImages[backgroundImageId%backgroundImages.length];
+    $.cookie('backgroundViewCount', ++backgroundViewCount);
+    currentBackgroundImageSrc = backgroundImages[backgroundViewCount%backgroundImages.length];
+    setBackgroundImage({'source':currentBackgroundImageSrc});
+}, 10000);
 
+$(window).resize(function() {
+    resizeText();
+});
+
+function setBackgroundImage(options) {
+    if (typeof options['source'] == 'undefined') { return false; }
+    var imageSource = options['source'];
+    var transition = typeof options['transition'] == 'undefined' || options['transition'] == true;
     var img = new Image();
     img.onload = function() {
         var oldCover = $('#cover-background-container');
         var newCover = oldCover.clone().hide();
         $('#cover-background-container').parent().append(newCover);
-        newCover.css('background-image', 'url(images/' + backgroundImageSrc+')')
+        newCover.css('background-image', 'url(images/' + imageSource+')')
         
-        oldCover.fadeOut(1500, function() {
-            oldCover.remove();    
-        });
-        newCover.fadeIn(1500);
+        if (transition) {
+            oldCover.fadeOut(1500, function() {
+                oldCover.remove();
+            });
+            newCover.fadeIn(1500, function() {
+                if (typeof options['callback'] != 'undefined') {
+                    options['callback']();
+                }
+            });
+        } else {
+            newCover.show();
+            oldCover.remove();
+            if (typeof options['callback'] != 'undefined') {
+                options['callback']();
+            }
+        }
     };
-    img.src = "images/" + backgroundImageSrc;
-}, 10000);
+    img.src = "images/" + imageSource;
+}
 
 function resizeText(firstPass) {
     $('body').css('font-size', ($('.content .name').innerWidth() / ($('.content .name .letter-spaced').html().length+1))/4);
@@ -41,7 +61,3 @@ function resizeText(firstPass) {
         resizeText(false);    
     }
 }
-
-$(window).resize(function() {
-    resizeText();
-});
